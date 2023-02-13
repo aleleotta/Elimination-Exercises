5eddb8ccc49210b9386cf42224bcef55f4f9c5c9
aleleotta <alessandro.leotta@iesnervion.es>
2023-02-13 13:46:09 +0100

    .

--- Elimination Exercises/src/exercise3/Functions.java
+++ Elimination Exercises/src/exercise3/Functions.java
@@ -4,12 +4,13 @@ import java.util.Arrays;
 
 public class Functions {
 	public static int[] eliminateValue(int[] array, int input) {
-		Arrays.sort(array);
-		int index = Arrays.binarySearch(array, 0);
-		if(index >= 0) {
-			System.arraycopy(array, index + 1, array, index, array.length - index - 1);
-			array = Arrays.copyOf(array, array.length - 1);
+		for(int index = 0; index < array.length; index++) {
+			int value = array[index];
+			if(index >= 0 && value == input) {
+				System.arraycopy(array, index + 1, array, index, array.length - index - 1);
+				array = Arrays.copyOf(array, array.length - 1);
 			}
+		}
 		return array;
 	}
 }
